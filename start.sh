de12ed495d232dec265a0a2ace9117a124716d84
migueltrader05 <migueltrader05@gmail.com>
2025-05-23 14:49:06 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-pip uninstall -y python-telegram-bot
-pip install python-telegram-bot==13.7 schedule requests
+pip install --upgrade --force-reinstall --no-cache-dir python-telegram-bot==13.7 schedule requests
+
+echo "✅ Bot de cupons rodando..."
 python bot_cupons_final.py
