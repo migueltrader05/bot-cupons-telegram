31a3053342fbdf70c4bda058b84bd31121f26972
migueltrader05 <migueltrader05@gmail.com>
2025-05-23 14:56:57 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-pip install --upgrade --force-reinstall --no-cache-dir python-telegram-bot==13.7 schedule requests
+echo "✅ Iniciando instalação das dependências..."
+pip install --upgrade pip
+pip install -r requirements.txt
 
-echo "✅ Iniciando o bot de cupons..."
+echo "✅ Iniciando o bot..."
 python bot_cupons_final.py
+
 
