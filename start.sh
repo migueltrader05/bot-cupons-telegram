b91d3c0eecfaec4bbcfea73726c2948e4f8258c6
migueltrader05 <migueltrader05@gmail.com>
2025-05-26 09:56:43 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-echo "✅ Iniciando instalação das dependências..."
-pip install --upgrade pip
 pip install -r requirements.txt
-
-echo "✅ Iniciando o bot..."
-python bot_cupons_final.py
-
-
+python3 bot_cupons_final.py
